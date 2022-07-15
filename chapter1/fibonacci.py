a5053fbc6bbe9314a91dcbb64ec11bef8eadfbf6
tjswodud <tjswodud85@gmail.com>
2022-07-15 17:21:42 +0900

    [NEW] fibonacci sequence algorithm

---
+++ chapter1/fibonacci.py
@@ -0,0 +1,18 @@
+"""
+Problem : 피보나치 수열의 n번째 수를 구하여라.
+Input : 음이 아닌 정수 n
+Output : 피보나치 수열의 n번째 수
+"""
+
+
+def fibonacci(n: int):
+    if n <= 1:
+        return n
+    else:
+        return fibonacci(n - 1) + fibonacci(n - 2)
+
+
+if __name__ == "__main__":
+    for i in range(1, 11):
+        print("fibonacci(%d) - %d" % (i, fibonacci(i)))
+        
